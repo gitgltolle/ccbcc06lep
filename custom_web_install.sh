ef739697c90140a48a9cf28785ce894bfb2ef5a0
gitgltolle <124804657+gitgltolle@users.noreply.github.com>
2023-03-14 15:13:16 +0000

    Update custom_web_install.sh
    Minor corrections

--- custom_web_install.sh
+++ custom_web_install.sh
@@ -1,4 +1,4 @@
 apt-get install -y nginx
-echo "MAIN $(hostname) | sudo tee /var/www/html/index.html
+echo "MAIN " $(hostname) | sudo tee /var/www/html/index.html
 sudo mkdir -p /var/www/html/images
-echo "IMAGES: " $(hostname) | sudo tee /var/www/html/images/test.html
+echo "IMAGES " $(hostname) | sudo tee /var/www/html/images/test.html
